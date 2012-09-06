17084f997ef1cdadeeaf492ae2347994e156309b
Matthew Daly <matthewbdaly@gmail.com>
2012-09-06 23:51:49 +0100

    Can now delete characters

--- js/main.js
+++ js/main.js
@@ -5,16 +5,30 @@ ShellSession.prototype.echoText = function(event) {
     // Prevent the default action
     event.preventDefault();
 
-    // Get the character from the key code
-    keypressed = String.fromCharCode(event.which);
-    var currenttext = $('#prompt').text();
-    $('#prompt').text(currenttext+keypressed);
+    // Get the key code
+    var keycode = event.which;
+    console.log(keycode);
+
+    // Capture characters
+    switch(keycode) {
+        case 8:
+            // Delete pressed - delete the last character
+            var consoletext = $('#prompt').text();
+            consoletext = consoletext.slice(0, (consoletext.length - 1));
+            $('#prompt').text(consoletext);
+            break;
+        default:
+            // Get the character from the key code
+            var keypressed = String.fromCharCode(keycode);
+            var currenttext = $('#prompt').text();
+            $('#prompt').text(currenttext+keypressed);
+    }
 }
 
 $(document).ready(function() {
     // Create a shell session object
     var shell = new ShellSession();
-    $(document).keypress(function(event){
+    $(document).keyup(function(event){
         shell.echoText(event);
     });
 });
