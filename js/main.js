1f3d68e66a3309ce75f293938c71dc0157ff47d7
Matthew Daly <matthewbdaly@gmail.com>
2012-09-06 23:17:45 +0100

    Can now enter text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+function ShellSession() {
+}
+
+ShellSession.prototype.echoText = function(event) {
+    // Prevent the default action
+    event.preventDefault();
+
+    // Get the character from the key code
+    keypressed = String.fromCharCode(event.which);
+    var currenttext = $('#prompt').text();
+    $('#prompt').text(currenttext+keypressed);
+}
+
 $(document).ready(function() {
-    // On load, focus on the prompt
-    $('#prompt').focus();
+    // Create a shell session object
+    var shell = new ShellSession();
+    $(document).keypress(function(event){
+        shell.echoText(event);
+    });
 });
