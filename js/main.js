59e002a6397b1e214027b9ff117c97e43407ebc5
Matthew Daly <matthewbdaly@gmail.com>
2012-09-07 20:11:27 +0100

    JS file now passes JSLint

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 function ShellSession() {
+    'use strict';
 }
 
-ShellSession.prototype.echoText = function(event) {
+ShellSession.prototype.echoText = function (event) {
+    'use strict';
+
     // Prevent the default action
     event.preventDefault();
 
+    // Define the variables used
+    var consoletext, keypressed, currenttext, keycode;
+
     // Get the key code
-    var keycode = event.which;
+    keycode = event.which;
 
     // Capture characters
-    switch(keycode) {
-        case 8:
-            // Delete pressed - delete the last character
-            var consoletext = $('#prompt').text();
-            consoletext = consoletext.slice(0, (consoletext.length - 1));
-            $('#prompt').text(consoletext);
-            break;
-        case 13:
-            // Enter pressed - move to next line
-            $('#prompt').after('<br /><span>shellshocked$ </span>');
-        default:
-            // Get the character from the key code
-            var keypressed = String.fromCharCode(keycode);
-            var currenttext = $('#prompt').text();
-            $('#prompt').text(currenttext+keypressed);
+    switch (keycode) {
+    case 8:
+        // Delete pressed - delete the last character
+        consoletext = $('#prompt').text();
+        consoletext = consoletext.slice(0, (consoletext.length - 1));
+        $('#prompt').text(consoletext);
+        break;
+    case 13:
+        // Enter pressed - move to next line
+        $('#prompt').after('<br /><span>shellshocked$ </span>');
+        break;
+    default:
+        // Get the character from the key code
+        keypressed = String.fromCharCode(keycode);
+        currenttext = $('#prompt').text();
+        $('#prompt').text(currenttext + keypressed);
     }
-}
+};
 
-$(document).ready(function() {
+$(document).ready(function () {
+    'use strict';
     // Create a shell session object
     var shell = new ShellSession();
-    $(document).keyup(function(event){
+    $(document).keyup(function (event) {
         shell.echoText(event);
     });
 });
