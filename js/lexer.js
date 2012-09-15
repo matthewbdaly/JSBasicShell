831e46cc298e97808fa21f6c90b5280d24d1cc58
Matthew Daly <matthewbdaly@gmail.com>
2012-09-15 23:35:52 +0100

    Added some tokens to the lexer

--- js/lexer.js
+++ js/lexer.js
@@ -1,5 +1,24 @@
 function Lexer() {
     'use strict';
+
+    // Define the tokens to match
+    // First get whitespace, number and word tokens
+    this.WHITESPACE = /\s+/;
+    this.NUMBER = /\d+/;
+    this.WORD = /\w+/;
+
+    // Define tokens for mathematical operators
+    this.ADD = /\+/;
+    this.SUBTRACT = /\-/;
+    this.MULTIPLY = /\*/;
+    this.DIVIDE = /\*/;
+    this.MODULO = /\%/;
+    this.EQUAL = /\=/;
+
+    // Define tokens for other characters
+    this.DOLLAR = /\$/;
+    this.SEMICOLON = /\;/;
+    this.COMMA = /\,/;
 }
 
 Lexer.prototype.getTokens = function (input) {
