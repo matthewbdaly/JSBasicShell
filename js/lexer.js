d16f76a6fccf8cf46f23737d0b0bdd3c98104875
Matthew Daly <matthewbdaly@gmail.com>
2012-09-17 20:36:39 +0100

    Now store the regexes in an object

--- js/lexer.js
+++ js/lexer.js
@@ -1,24 +1,26 @@
 function Lexer() {
     'use strict';
 
-    // Define the tokens to match
-    // First get whitespace, number and word tokens
-    this.WHITESPACE = /^\s+/;
-    this.NUMBER = /^\d+/;
-    this.WORD = /^\w+/;
+    // Store tokens as key-value pairs
+    this.tokens = {};
+    
+    // Define whitespace, number and word tokens
+    this.tokens.whitespace = /^\s+/;
+    this.tokens.number = /^\d+/;
+    this.tokens.word = /^\w+/;
 
     // Define tokens for mathematical operators
-    this.ADD = /^\+/;
-    this.SUBTRACT = /^\-/;
-    this.MULTIPLY = /^\*/;
-    this.DIVIDE = /^\*/;
-    this.MODULO = /^\%/;
-    this.EQUAL = /^\=/;
+    this.tokens.add = /^\+/;
+    this.tokens.subtract = /^\-/;
+    this.tokens.multiply = /^\*/;
+    this.tokens.divide = /^\*/;
+    this.tokens.modulo = /^\%/;
+    this.tokens.equal = /^\=/;
 
     // Define tokens for other characters
-    this.DOLLAR = /^\$/;
-    this.SEMICOLON = /^\;/;
-    this.COMMA = /^\,/;
+    this.tokens.dollar = /^\$/;
+    this.tokens.semicolon = /^\;/;
+    this.tokens.comma = /^\,/;
 }
 
 Lexer.prototype.getTokens = function (input) {
