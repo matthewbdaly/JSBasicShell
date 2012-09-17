1a90232899f1671d9ace4c238cbdc4d754622912
Matthew Daly <matthewbdaly@gmail.com>
2012-09-17 22:16:03 +0100

    Parser now picks up all the info needed

--- js/parser.js
+++ js/parser.js
@@ -4,4 +4,17 @@ function Parser() {
 
 Parser.prototype.processTokens = function (input) {
     'use strict';
+
+    // Define variables
+    var item, output, token, tokens = [];
+
+    // Get the tokens
+    for (item in input) {
+        if (input.hasOwnProperty(item)) {
+            token = {};
+            token.text = input[item].tokenvalue[0];
+            token.tokentype = input[item].tokentype;
+            tokens.push(token);
+        }
+    }
 };
