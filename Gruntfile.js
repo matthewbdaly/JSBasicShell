2522f7670cbf61716d51f591c219c6fd9bedd2f9
Matthew Daly <matthewbdaly@gmail.com>
2013-11-09 23:25:38 +0000

    Now run tests with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        jasmine: {
+            jsbasicshell: {
+                src: [
+                    'static/js/shell.js',
+                    'static/js/lexer.js',
+                    'static/js/parser.js',
+                    'static/js/main.js'
+                ]
+            },
+            options: {
+                vendor: [
+                    'static/bower_components/requirejs/require.js',
+                    'static/bower_components/jquery/jquery.min.js'
+                ],
+                specs: 'tests/test.js'
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    // Register tasks
+    grunt.registerTask('test', 'jasmine');
+};
