4ee675a6c532315475ad8c86a15fe9470aedd1d2
Matthew Daly <matthewbdaly@gmail.com>
2013-11-13 21:29:57 +0000

    Extended lexer test

--- tests/lexer.js
+++ tests/lexer.js
@@ -11,5 +11,32 @@ describe("Test lexer", function () {
 
         // Ensure tokens created
         expect(lexer.tokens).toBeDefined();
+        expect(lexer.tokens.whitespace).toBeDefined();
+        expect(lexer.tokens.number).toBeDefined();
+        expect(lexer.tokens.word).toBeDefined();
+        expect(lexer.tokens.add).toBeDefined();
+        expect(lexer.tokens.subtract).toBeDefined();
+        expect(lexer.tokens.multiply).toBeDefined();
+        expect(lexer.tokens.divide).toBeDefined();
+        expect(lexer.tokens.modulo).toBeDefined();
+        expect(lexer.tokens.equal).toBeDefined();
+        expect(lexer.tokens.leftbracket).toBeDefined();
+        expect(lexer.tokens.rightbracket).toBeDefined();
+        expect(lexer.tokens.lt).toBeDefined();
+        expect(lexer.tokens.gt).toBeDefined();
+        expect(lexer.tokens.dollar).toBeDefined();
+        expect(lexer.tokens.semicolon).toBeDefined();
+        expect(lexer.tokens.comma).toBeDefined();
+        expect(lexer.tokens.singlequotes).toBeDefined();
+        expect(lexer.tokens.doublequotes).toBeDefined();
+        expect(lexer.tokens.hash).toBeDefined();
+        expect(lexer.tokens.bang).toBeDefined();
+        expect(lexer.tokens.amp).toBeDefined();
+        expect(lexer.tokens.pipe).toBeDefined();
+        expect(lexer.tokens.tilde).toBeDefined();
+        expect(lexer.tokens.backslash).toBeDefined();
+        expect(lexer.tokens.dot).toBeDefined();
+        expect(lexer.tokens.colon).toBeDefined();
+        expect(lexer.tokens.unwanted).toBeDefined();
     });
 });
